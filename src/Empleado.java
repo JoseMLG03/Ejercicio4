697af8e07a1b49faaabc0f47252ca7d38ee92435
Jose <Jose.lomba.goce03@gmail.com>
2023-05-24 18:36:54 +0200

    ahora hay dos campos inmutables

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,16 +1,21 @@
 import java.util.Date;
 
-class Empleado {
-    final String nombre;
-    Date birthDate;
+import java.util.Date;
 
-    Empleado(String name, Date birthDate) {
-        this.nombre = name;
-        this.birthDate = birthDate;
+class Empleado {
+    String nombre;
+    final String nombre;
+    int edad;
+    Date fechaContratacion;
+    final Date fechaContratacion;
+
+    public Empleado(String nombre, int edad, Date fechaContratacion) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.fechaContratacion = fechaContratacion;
     }
     @Override
     public String toString() {
-        return "Empleado: " + nombre + " (nacido en 19" + birthDate.getYear() + ")";
+        return "Se ha registrado a " + nombre + " (" + edad + " años; contratado en 19" + fechaContratacion.getYear() + ")";
     }
-
 }
