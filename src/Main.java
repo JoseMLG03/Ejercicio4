1099bb88ce68f3dcd601db5a26aa15e5d0e7ad63
Jose <Jose.lomba.goce03@gmail.com>
2023-05-24 16:50:54 +0200

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    import java.util.Calendar;
+    import java.util.Date;
+
+    public class Main {
+        public static void main(String[] args) {
+
+
+            Persona persona = new Persona();
+            persona.nombre = "Pepe Pérez";
+            persona.edad = 31;
+            persona.fechaContratacion = new Date(90, Calendar.FEBRUARY, 1);
+
+            System.out.println("Se ha registrado a " + persona.nombre + " (" + persona.edad + " años; contratado en 19" + persona.fechaContratacion.getYear() + ")");
+        }
     }
-}
